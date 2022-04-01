09f621888ee34632f7d0693ec50df9b61cd56d9f
Kapil Bhardwaj (INDIAN) <91521935+Kapil-Bhardwaj@users.noreply.github.com>
2022-04-01 16:19:26 +0530

    account class

---
+++ Java Programming Language TCS-408/Java Programs/Chapter 1 (Basic Introduction)/Heirarical_Inhertance/account.java
@@ -0,0 +1,14 @@
+package Hierarical;
+
+ class account {
+    double accno;
+    String name;
+    int balance;
+    void Get_Account_info(double acc_no,String name , int bal )
+    {
+        accno = acc_no;
+        this.name = name;
+           balance = bal;
+    }
+
+}
