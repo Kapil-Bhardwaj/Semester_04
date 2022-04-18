526e0d805c1aa3394b0f2b8359ec82b2d92a48fe
Kapil Bhardwaj (INDIAN) <91521935+Kapil-Bhardwaj@users.noreply.github.com>
2022-04-18 12:02:24 +0530

    use of final keyword in variable

---
+++ Java Programming Language TCS-408/Java Programs/Chapter 2/finalVeriable.java
@@ -0,0 +1,14 @@
+class finalVariable
+{
+    public static void main(String[] args) {
+        int a = 90;
+        a = a+90;
+    System.out.println(a);
+
+//     this wil return an error  : cannot assign a value to final variable b
+//    final int b = 90;
+//     b = b+90;
+// System.out.println(b);
+
+    }
+}
