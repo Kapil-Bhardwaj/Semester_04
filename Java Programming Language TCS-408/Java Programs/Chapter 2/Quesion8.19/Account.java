2602dd597a4bf4f2fb492d8ba2e90a24c1a04531
Kapil Bhardwaj (INDIAN) <91521935+Kapil-Bhardwaj@users.noreply.github.com>
2022-05-05 11:46:01 +0530

    Account class

---
+++ Java Programming Language TCS-408/Java Programs/Chapter 2/Quesion8.19/Account.java
@@ -0,0 +1,17 @@
+package Ques8;
+
+public class Account {
+    
+    String name;
+    double Ano;
+    char accountType;
+    int bal;
+    int intrest ;
+    void getDetail(String name , double Ano , int bal  )
+    {
+          this.name =name;
+          this.Ano = Ano;
+          this.bal = bal; 
+    }
+
+}
