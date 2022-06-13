ac6f493de0d37225d5045e389342bc3c86c8bda9
Kapil Bhardwaj (INDIAN) <91521935+Kapil-Bhardwaj@users.noreply.github.com>
2022-06-13 10:23:57 +0530

    GUI  demo

---
+++ Java Programming Language TCS-408/Java Programs/AWT & Swings/app.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+class app
+{
+ app()
+   {
+      JFrame j = new JFrame();
+      j.setSize(300,300);       
+ j.setVisible(true);
+     }
+
+   public static void main(String[] args)  
+    {
+              app A = new app();
+               
+    }
+
+}
+
